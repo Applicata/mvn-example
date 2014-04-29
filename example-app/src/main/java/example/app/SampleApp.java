88ac5c5ec265edd0f4afde6bb6bb2fb367132922
Svilen Ivanov <isvilen@applicata.bg>
2014-04-29 11:26:05 +0300

    Add sample source code.

---
+++ example-app/src/main/java/example/app/SampleApp.java
@@ -0,0 +1,10 @@
+package example.app;
+
+import example.lib.Sample;
+
+public class SampleApp {
+  public static void main(String[] args) {
+    Sample sample = new Sample();
+    sample.method();
+  }
+}
---
+++ example-lib/src/main/java/example/lib/Sample.java
@@ -0,0 +1,7 @@
+package example.lib;
+
+public class Sample {
+  public void method() {
+    System.out.println("Sample.method()");
+  }
+}
