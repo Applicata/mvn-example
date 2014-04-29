2b5e59838c05bc83ed066507b736900c0ea65954
Svilen Ivanov <isvilen@applicata.bg>
2014-04-29 11:35:41 +0300

    Add sample tests.

---
+++ example-app/src/test/java/example/app/SampleAppTest.java
@@ -0,0 +1,13 @@
+package example.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleAppTest {
+
+  @Test()
+  public void testSampleApp1() {
+    assertTrue(true);
+  }
+}
---
+++ example-lib/src/test/java/example/lib/SampleTest.java
@@ -0,0 +1,13 @@
+package example.lib;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleTest {
+
+  @Test()
+  public void testSample1() {
+    assertTrue(true);
+  }
+}
