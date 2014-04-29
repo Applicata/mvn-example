7128b66dd60f69417240350b17642683953028c2
Svilen Ivanov <isvilen@applicata.bg>
2014-04-29 13:52:06 +0300

    Separate unit and integration tests.

---
+++ example-app/src/test/java/example/app/SampleAppITCase.java
@@ -0,0 +1,13 @@
+package example.app;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class SampleAppITCase {
+
+  @Test()
+  public void testSampleAppITest1() {
+    assertTrue(true);
+  }
+}
